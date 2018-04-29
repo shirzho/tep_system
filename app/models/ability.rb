ff6fbb9825d21d4422ae9082130b14ddd0ebf7ed
Shirley Zhou <Shirley@Shirleys-MacBook-Pro.local>
2018-04-28 21:49:19 -0400

    added abilities for different user roles

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,27 +7,22 @@ class Ability
     user ||= User.new # guest user (not logged in)
     if user.role? :admin
         can :manage, :all
+
+    elsif user.role? :volunteer
+        can :create, Order do |order|
+            order.id == user.order_id
+        end
+        can :create, OrderItem do |order_item|
+            order_item.id == user.order_item
+        end
+    elsif user.role? :manager
+        can :create, Order
+        can :update, Item 
+        can :read, School
+        can :read, OrderItems
+        can :read, Teachers
     else
-        can :read, :all
+        can :read, Home
     end
-    
-    
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
   end
 end
